c778a6a01b78a7c68f9d2dd7c4b91624240eddef
orodrakon <orodrakon@gmail.com>
2026-02-28 17:31:48 -0500

    Does Camera Work Now

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var current_tool: DataTypes.Action = DataTypes.Action.Idle
 @export var animate: AnimatedSprite2D
 
+
 const SPEED = 130.0
 const JUMP_VELOCITY = -400.0
 
@@ -23,16 +24,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = y_direction * SPEED
 	else:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
-
+		
+	if x_direction == 0 and y_direction == 0:
+		animate.play("idle")
+		
 	if velocity.y < 0:
-		$AnimatedSprite2D2.play("up")
+		animate.play("up")
 	elif velocity.y > 0:
-		$AnimatedSprite2D2.play("down")
+		animate.play("down")
 	
 	if velocity.x < 0:
-		$AnimatedSprite2D2.play("left")
+		animate.play("left")
 	elif velocity.x > 0:
-		$AnimatedSprite2D2.play("right")
+		animate.play("right")
 	#if (not velocity.x or not velocity.y):
 	#	$AnimatedSprite2D2.play("idle")
 	
