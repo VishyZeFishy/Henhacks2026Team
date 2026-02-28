57be96ca45fae4db0055f639ee267ab320992e89
agcud <agcastre@udel.edu>
2026-02-28 15:15:29 -0500

    PLAYER + MOVEMENT IMPLEMENTATION

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var y_direction := Input.get_axis("up", "down")
+	var x_direction := Input.get_axis("left", "right")
+	
+	#Y DIRECTION
+	if x_direction:
+		velocity.x = x_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+	#Y DIRECTION
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
